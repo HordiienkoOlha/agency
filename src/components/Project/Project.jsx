c2cd507c82ff787a9c9b86ddedd8017105e336ca
HordiienkoOlha <olhahordiienkomail@gmail.com>
2023-08-09 14:09:11 +0300

    update Hero.module.css, Project.jsx

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -7,23 +7,25 @@ import Card from "../Card/Card";
 function Project() {
   return (
     <section className={s.container}>
-      <ProjectImage />
-      <div className={s.content}>
-        <div className={s.wrapperList}>
-          <ul className={s.list}>
-            <li>
-              <Star />
-            </li>
-            <li className={s.item}>
-              <h2 className={s.title}>Great Project</h2>
-            </li>
-          </ul>
-          <p className={s.preText}>
-            800+ <span className={s.text}>Done</span>
-          </p>
+      <div className={s.project}>
+        <ProjectImage />
+        <div className={s.content}>
+          <div className={s.wrapperList}>
+            <ul className={s.list}>
+              <li>
+                <Star />
+              </li>
+              <li className={s.item}>
+                <h2 className={s.title}>Great Project</h2>
+              </li>
+            </ul>
+            <p className={s.preText}>
+              800+ <span className={s.text}>Done</span>
+            </p>
+          </div>
         </div>
+        <Card />
       </div>
-      <Card />
     </section>
   );
 }
